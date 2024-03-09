53d420716a7d32db72390a964f45c56463f5e085
yuji-kazama <y-kazama@mercari.com>
2024-03-10 08:32:55 +0900

    fix xpath

--- main.go
+++ main.go
@@ -1,104 +1,105 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"strings"
+  "context"
+  "fmt"
+  "io"
+  "net/http"
+  "os"
+  "strings"
 
-	"github.com/chromedp/cdproto/cdp"
-	"github.com/chromedp/chromedp"
+  "github.com/chromedp/cdproto/cdp"
+  "github.com/chromedp/chromedp"
 )
 
-	const (
-		usageNotice = "Usage: go run main.go <NativeCamp_DailyNews_Page_URLs>"
-		audioXPath = "/html/body/div[4]/div/div/div/div/article/div[1]/div[8]/div/div[2]/div/div[2]/p/a" 
-		folderPath = "./out"
-	)
+  const (
+    usageNotice = "Usage: go run main.go <NativeCamp_DailyNews_Page_URLs>"
+    audioXPath = "/html/body/div[4]/div/div/div/div/article/div[1]/div[8]/div/div[2]/div/div[2]/div/button"
+    // audioXPath = "/html/body/div[4]/div/div/div/div/article/div[1]/div[8]/div/div[2]/div/div[2]/p/a"
+    folderPath = "./out"
+  )
 
 func main() {
-	totalArg := len(os.Args)
-	if totalArg < 2 {
-		fmt.Println(usageNotice)
-		return
-	}
-	for i := 1; i < totalArg; i++ {
-		pageURL := os.Args[i]
-		fmt.Printf("[%d/%d] %s\n", i, totalArg-1, pageURL)
-		audioFileURL, err := getAudioFileURL(pageURL)
-		if err != nil {
-			fmt.Println("Failed to get audio file URL:", err)
-			return
-		}
-		err = downloadFile(audioFileURL)
-		if err != nil {
-			fmt.Println("Failed to downlod file:", err)
-			return
-		}
-	}
+  totalArg := len(os.Args)
+  if totalArg < 2 {
+    fmt.Println(usageNotice)
+    return
+  }
+  for i := 1; i < totalArg; i++ {
+    pageURL := os.Args[i]
+    fmt.Printf("[%d/%d] %s\n", i, totalArg-1, pageURL)
+    audioFileURL, err := getAudioFileURL(pageURL)
+    if err != nil {
+      fmt.Println("Failed to get audio file URL:", err)
+      return
+    }
+    err = downloadFile(audioFileURL)
+    if err != nil {
+      fmt.Println("Failed to downlod file:", err)
+      return
+    }
+  }
 }
 
 func getAudioFileURL(pageURL string) (string, error) {
-	ctx, cancel := chromedp.NewContext(context.Background())
-	defer cancel()
+  ctx, cancel := chromedp.NewContext(context.Background())
+  defer cancel()
 
-	var nodes []*cdp.Node
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(pageURL),
-		chromedp.WaitVisible(audioXPath, chromedp.BySearch),
-		chromedp.Nodes(audioXPath, &nodes, chromedp.BySearch),
-	)
-	if err != nil {
-		return "", fmt.Errorf("failed to execute ChromeDP: %w", err)
-	}
+  var nodes []*cdp.Node
+  err := chromedp.Run(ctx,
+    chromedp.Navigate(pageURL),
+    chromedp.WaitVisible(audioXPath, chromedp.BySearch),
+    chromedp.Nodes(audioXPath, &nodes, chromedp.BySearch),
+  )
+  if err != nil {
+    return "", fmt.Errorf("failed to execute ChromeDP: %w", err)
+  }
 
-	return nodes[0].AttributeValue("data-src"), nil
+  return nodes[0].AttributeValue("data-src"), nil
 }
 
 func downloadFile(fileURL string) error {
-	resp, err := http.Get(fileURL)
-	if err != nil {
-		return fmt.Errorf("failed to download file: %w", err)
-	}
-	defer resp.Body.Close()
+  resp, err := http.Get(fileURL)
+  if err != nil {
+    return fmt.Errorf("failed to download file: %w", err)
+  }
+  defer resp.Body.Close()
 
-	err = createDirectoryIfNotExist(folderPath)
-	if err != nil {
-		return fmt.Errorf("failed to create directory: %w", err)
-	}
+  err = createDirectoryIfNotExist(folderPath)
+  if err != nil {
+    return fmt.Errorf("failed to create directory: %w", err)
+  }
 
-	fileName := getDownloadableFileName(fileURL)
-	filePath := folderPath + "/" + fileName
-	out, err := os.Create(filePath)
-	if err != nil {
-		return fmt.Errorf("failed to create file: %w", err)
-	}
-	defer out.Close()
+  fileName := getDownloadableFileName(fileURL)
+  filePath := folderPath + "/" + fileName
+  out, err := os.Create(filePath)
+  if err != nil {
+    return fmt.Errorf("failed to create file: %w", err)
+  }
+  defer out.Close()
 
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return fmt.Errorf("failed to save file: %w", err)
-	}
-	return nil
+  _, err = io.Copy(out, resp.Body)
+  if err != nil {
+    return fmt.Errorf("failed to save file: %w", err)
+  }
+  return nil
 }
 
 func createDirectoryIfNotExist(dirPath string) error {
-	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
-		err := os.Mkdir(dirPath, 0755)
-		if err != nil {
-			return fmt.Errorf("failed to create directory: %w", err)
-		}
-	}
-	return nil
+  if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+    err := os.Mkdir(dirPath, 0755)
+    if err != nil {
+      return fmt.Errorf("failed to create directory: %w", err)
+    }
+  }
+  return nil
 }
 
 func getDownloadableFileName(fileURL string) string {
-	keyword := "/"
-	index := strings.LastIndex(fileURL, keyword)
-	if index < 0 {
-		return fileURL
-	}
-	return fileURL[index+len(keyword):]
+  keyword := "/"
+  index := strings.LastIndex(fileURL, keyword)
+  if index < 0 {
+    return fileURL
+  }
+  return fileURL[index+len(keyword):]
 }
--- run.sh
+++ run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 PAGE_URLS="
- https://nativecamp.net/textbook/page-detail/2/20468
- https://nativecamp.net/textbook/page-detail/2/20433
- https://nativecamp.net/textbook/page-detail/2/20493
- https://nativecamp.net/textbook/page-detail/2/20553
+https://nativecamp.net/textbook/page-detail/2/24193
+https://nativecamp.net/textbook/page-detail/2/24163
+https://nativecamp.net/textbook/page-detail/2/24177
+https://nativecamp.net/textbook/page-detail/2/24172
+https://nativecamp.net/textbook/page-detail/2/24189
 "
+
 rm -rf ./out
 go run main.go $PAGE_URLS
 # ./ncfiledownloader $PAGE_URLS
